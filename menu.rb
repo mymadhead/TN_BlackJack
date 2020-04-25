bdfbbd93b6f424decb597119e1213c9e9eab8cad
mymadhead <mymadhead@gmail.com>
2020-04-25 15:58:21 +0300

    Add menu module

---
+++ menu.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Menu
+
+  def welcome
+    puts 'Hello! This is a Black Jack game!'
+    puts 'What is your name?'
+  end
+
+  def shuffling
+    puts 'Cards are shuffling, please wait!'
+  end
+
+end
